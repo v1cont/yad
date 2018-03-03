a26fd6b254a1099551117b7be0c8e0a38677e43a
Victor Ananjesky <ananasik@gmail.com>
2018-03-03 21:36:35 +0200

    fix copyright years

--- src/browser.c
+++ src/browser.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with YAD. If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) 2010-2017, Victor Ananjevsky <ananasik@gmail.com>
+ * Copyright (C) 2010-2018, Victor Ananjevsky <ananasik@gmail.com>
  */
 
 #include <config.h>
