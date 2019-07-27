9d1486d8cf6d03f4602b6309e65ae9c3ac6957f2
Victor Ananjevsky <ananasik@gmail.com>
2019-07-27 13:32:13 +0300

    rename --add-action to --row-action and extend it for other row actions (edit and remove)

--- src/yad.h
+++ src/yad.h
@@ -345,7 +345,7 @@ typedef struct {
   gchar *ellipsize_cols;
   gchar *dclick_action;
   gchar *select_action;
-  gchar *add_action;
+  gchar *row_action;
   gboolean regex_search;
   gboolean clickable;
   gboolean no_selection;
