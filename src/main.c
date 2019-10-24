6cca1775eb62795bc64a6be91024f17a4a9d62db
step- <step-@users.noreply.github.com>
2019-10-24 23:40:54 +0200

    6 fix --version exit status

--- src/main.c
+++ src/main.c
@@ -901,6 +901,7 @@ main (gint argc, gchar ** argv)
 
     case YAD_MODE_VERSION:
       g_print ("%s (GTK+ %d.%d.%d)\n", VERSION, gtk_major_version, gtk_minor_version, gtk_micro_version);
+      ret = 0;
       break;
 
 #ifdef HAVE_SPELL
