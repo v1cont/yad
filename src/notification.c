v1cont/yad

commit 26c27b46779d886c3de6f309777f539bbe383e5e

remove gtk2 code

src/notification.c
@@ -305,28 +305,10 @@ handle_stdin (GIOChannel * channel, GIOCondition condition, gpointer data)
             }
           else if (!g_ascii_strcasecmp (command, "visible"))
             {
-#if !GTK_CHECK_VERSION(2,22,0)
-              if (!g_ascii_strcasecmp (value, "blink"))
-                {
-                  gboolean state = gtk_status_icon_get_blinking (status_icon);
-                  gtk_status_icon_set_blinking (status_icon, !state);
-                }
-              else
-#endif
               if (!g_ascii_strcasecmp (value, "false"))
-                {
-                  gtk_status_icon_set_visible (status_icon, FALSE);
-#if !GTK_CHECK_VERSION(2,22,0)
-                  gtk_status_icon_set_blinking (status_icon, FALSE);
-#endif
-                }
+                gtk_status_icon_set_visible (status_icon, FALSE);
               else
-                {
-                  gtk_status_icon_set_visible (status_icon, TRUE);
-#if !GTK_CHECK_VERSION(2,22,0)
-                  gtk_status_icon_set_blinking (status_icon, FALSE);
-#endif
-                }
+                gtk_status_icon_set_visible (status_icon, TRUE);
             }
           else if (!g_ascii_strcasecmp (command, "action"))
             {

acinclude.m4
@@ -1,17 +1,0 @@
-dnl Disable deprecated GTK+ features
-AC_DEFUN([GTK_DISABLE_DEPRECATED],
-[
-	AC_ARG_ENABLE(deprecated, [AC_HELP_STRING([--disable-deprecated],
-					[Disable deprecated GTK functions])], 
-			,[enable_deprecated=yes])
-
-
-	if test x$enable_deprecated = xyes
-	then
-		DISABLE_DEPRECATED=
-	else
-		DISABLE_DEPRECATED="-DG_DISABLE_DEPRECATED -DGDK_DISABLE_DEPRECATED -DGDK_PIXBUF_DISABLE_DEPRECATED -DPANGO_DISABLE_DEPRECATED -DGTK_DISABLE_DEPRECATED -DGSEAL_ENABLE"
-	fi
-	
-	AC_SUBST(DISABLE_DEPRECATED)
-])

configure.ac
@@ -1,4 +1,4 @@
-AC_INIT([YAD], [0.42.0], [https://sourceforge.net/p/yad-dialog/tickets/], yad, [https://sourceforge.net/projects/yad-dialog/])
+AC_INIT([YAD], [0.99], [https://github.com/v1cont/yad/issues/], yad, [https://github.com/v1cont/yad/])
 AC_CONFIG_AUX_DIR([build])
 
 AM_INIT_AUTOMAKE([1.11 foreign dist-xz no-dist-gzip])
@@ -15,37 +15,9 @@ IT_PROG_INTLTOOL([0.40.0])
 
 AC_PROG_CC
 
-AC_ARG_WITH([gtk],
-	[AS_HELP_STRING([--with-gtk=gtk2|gtk3],[set the GTK+ version to use (default - gtk2)])],
-	[case "$with_gtk" in
-        gtk2|gtk3) ;;
-        *) AC_MSG_ERROR([invalid gtk version specified]) ;;
-     esac],
-    [with_gtk=gtk2])
-
-case "$with_gtk" in
-    gtk3) 
-        GTK_MODULE="gtk+-3.0"
-        GTK_REQUIRED="3.0.0"
-        GTK_PRINT="gtk+-unix-print-3.0"
-        WEBKIT="webkit2gtk-4.0"
-        SPELL="gtkspell3-3.0"
-        SOURCEVIEW="gtksourceview-3.0"
-        ;;
-    gtk2)
-        GTK_MODULE="gtk+-2.0"
-        GTK_REQUIRED="2.16.0"
-        GTK_PRINT="gtk+-unix-print-2.0"
-        WEBKIT="webkit-1.0"
-        SPELL="gtkspell3-2.0"
-        SOURCEVIEW="gtksourceview-2.0"
-        ;;
-esac
-PKG_CHECK_MODULES([GTK], [$GTK_MODULE >= $GTK_REQUIRED $GTK_PRINT])
+PKG_CHECK_MODULES([GTK], [gtk+-3.0 >= 3.22.0 gtk+-unix-print-3.0])
 AC_SUBST([GTK_CFLAGS])
 AC_SUBST([GTK_LIBS])
-
-GTK_DISABLE_DEPRECATED
 
 dnl rgb.txt
 AC_ARG_WITH([rgb],
@@ -60,7 +32,7 @@ AC_ARG_ENABLE([html],
 			[Build YAD with HTML widget])],
 	[build_html=$enableval], [build_html=yes])
 if test x$build_html = xyes; then
-    PKG_CHECK_MODULES([HTML], [$WEBKIT], [have_html=yes], [have_html=no])
+    PKG_CHECK_MODULES([HTML], [webkit2gtk-4.0], [have_html=yes], [have_html=no])
 else
     have_html=no
 fi
@@ -69,10 +41,7 @@ AC_SUBST([HTML_LIBS])
 AM_CONDITIONAL([HTML], [test x$have_html = xyes])
 
 if test x$have_html = xyes; then
-    AC_DEFINE([HAVE_HTML], [1], [Define this if you have webkit installed])
-    if test x$with_gtk = xgtk3; then
-        AC_DEFINE([USE_WEBKIT2], [1], [Define this if you use webkit2 API])
-    fi
+    AC_DEFINE([HAVE_HTML], [1], [Define this if you have webkit2gt library])
 fi
 
 dnl gio support
@@ -95,10 +64,10 @@ fi
 dnl spell check
 AC_ARG_ENABLE([spell],
 	[AS_HELP_STRING([--enable-spell],
-			[Build YAD with Spell check support (default - no)])],
-	[with_spell=$enableval], [with_spell=no])
+			[Build YAD with Spell check support (default - yes)])],
+	[with_spell=$enableval], [with_spell=yes])
 if test x$with_spell = xyes; then
-    PKG_CHECK_MODULES([SPELL], [$SPELL], [have_spell=yes], [have_spell=no])
+    PKG_CHECK_MODULES([SPELL], [gtkspell-3.0], [have_spell=yes], [have_spell=no])
 else
     have_spell=no
 fi
@@ -112,10 +81,10 @@ fi
 dnl sourceview
 AC_ARG_ENABLE([sourceview],
 	[AS_HELP_STRING([--enable-sourceview],
-			[Build YAD with GtkSourceView support (default - no)])],
-	[with_sourceview=$enableval], [with_sourceview=no])
+			[Build YAD with GtkSourceView support (default - yes)])],
+	[with_sourceview=$enableval], [with_sourceview=yes])
 if test x$with_sourceview = xyes; then
-    PKG_CHECK_MODULES([SOURCEVIEW], [$SOURCEVIEW], [have_sourceview=yes], [have_sourceview=no])
+    PKG_CHECK_MODULES([SOURCEVIEW], [gtksourceview-3.0], [have_sourceview=yes], [have_sourceview=no])
 else
     have_sourceview=no
 fi
@@ -183,12 +152,11 @@ AC_OUTPUT
 
 echo
 echo "Build configuratioh:"
-echo "  GTK+ version         - $with_gtk"
-echo "  Path to rgb.txt      - $with_rgb"
 echo "  HTML widget          - $have_html"
-echo "  Spell checking       - $have_spell"
 echo "  GtkSourceView        - $have_sourceview"
+echo "  Spell checking       - $have_spell"
 echo "  GIO support          - $have_gio"
+echo "  Path to rgb.txt      - $with_rgb"
 echo "  pfd                  - $build_pfd"
 echo "  Icon browser         - $build_ib"
 echo

src/Makefile.am
@@ -1,6 +1,5 @@
 AM_CPPFLAGS =                           \
-	-DLOCALEDIR=\""$(datadir)/locale"\"	\
-	@DISABLE_DEPRECATED@
+	-DLOCALEDIR=\""$(datadir)/locale"\"
 
 bin_PROGRAMS = yad
 

src/browser.c
@@ -40,11 +40,7 @@ typedef struct {
 static gboolean
 key_press_cb (GtkWidget * w, GdkEventKey * ev, gpointer data)
 {
-#if GTK_CHECK_VERSION(2,24,0)
   if (ev->keyval == GDK_KEY_Escape)
-#else
-  if (ev->keyval == GDK_Escape)
-#endif
     {
       gtk_main_quit ();
       return TRUE;
@@ -144,11 +140,7 @@ select_icon (GtkTreeSelection * sel, IconBrowserData * data)
   g_string_free (sizes, TRUE);
 
   if (info)
-#if !GTK_CHECK_VERSION(3,0,0)
-    gtk_icon_info_free(info);
-#else
     g_object_unref (info);
-#endif
 }
 
 static void
@@ -220,99 +212,54 @@ main (gint argc, gchar * argv[])
   g_signal_connect (G_OBJECT (data->win), "delete-event", G_CALLBACK (gtk_main_quit), NULL);
   g_signal_connect (G_OBJECT (data->win), "key-press-event", G_CALLBACK (key_press_cb), NULL);
 
-#if !GTK_CHECK_VERSION(3,0,0)
-  box = gtk_vbox_new (FALSE, 5);
-#else
   box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 5);
-#endif
   gtk_container_add (GTK_CONTAINER (data->win), box);
   gtk_container_set_border_width (GTK_CONTAINER (data->win), 5);
 
   /* create icon info box */
-#if !GTK_CHECK_VERSION(3,0,0)
-  t = gtk_table_new (3, 3, FALSE);
-  gtk_table_set_col_spacings (GTK_TABLE (t), 5);
-  gtk_table_set_row_spacings (GTK_TABLE (t), 5);
-#else
   t = gtk_grid_new ();
   gtk_grid_set_row_spacing (GTK_GRID (t), 5);
   gtk_grid_set_column_spacing (GTK_GRID (t), 5);
-#endif
   gtk_box_pack_start (GTK_BOX (box), t, FALSE, FALSE, 2);
 
   data->image = gtk_image_new_from_icon_name ("image-missing", GTK_ICON_SIZE_DIALOG);
-#if !GTK_CHECK_VERSION(3,0,0)
-  gtk_table_attach (GTK_TABLE (t), data->image, 0, 1, 0, 3, GTK_FILL, 0, 0, 0);
-#else
   gtk_grid_attach (GTK_GRID (t), data->image, 0, 0, 1, 3);
-#endif
 
   w = gtk_label_new (NULL);
   gtk_label_set_markup (GTK_LABEL (w), _("<b>Name:</b>"));
-#if !GTK_CHECK_VERSION(3,0,0)
-  gtk_misc_set_alignment (GTK_MISC (w), 0.0, 0.5);
-  gtk_table_attach (GTK_TABLE (t), w, 1, 2, 0, 1, GTK_FILL, 0, 0, 0);
-#else
   gtk_label_set_xalign (GTK_LABEL (w), 0.0);
   gtk_grid_attach (GTK_GRID (t), w, 1, 0, 1, 1);
-#endif
+
   data->lname = gtk_label_new (NULL);
   gtk_label_set_selectable (GTK_LABEL (data->lname), TRUE);
-#if !GTK_CHECK_VERSION(3,0,0)
-  gtk_misc_set_alignment (GTK_MISC (data->lname), 0.0, 0.5);
-  gtk_table_attach (GTK_TABLE (t), data->lname, 2, 3, 0, 1, GTK_FILL | GTK_EXPAND, 0, 0, 0);
-#else
   gtk_label_set_xalign (GTK_LABEL (data->lname), 0.0);
   gtk_grid_attach (GTK_GRID (t), data->lname, 2, 0, 1, 1);
   gtk_widget_set_hexpand (data->lname, TRUE);
-#endif
 
   w = gtk_label_new (NULL);
   gtk_label_set_markup (GTK_LABEL (w), _("<b>Sizes:</b>"));
-#if !GTK_CHECK_VERSION(3,0,0)
-  gtk_misc_set_alignment (GTK_MISC (w), 0.0, 0.5);
-  gtk_table_attach (GTK_TABLE (t), w, 1, 2, 1, 2, GTK_FILL, 0, 0, 0);
-#else
   gtk_label_set_xalign (GTK_LABEL (w), 0.0);
   gtk_grid_attach (GTK_GRID (t), w, 1, 1, 1, 1);
-#endif
+
   data->lsize = gtk_label_new (NULL);
   gtk_label_set_selectable (GTK_LABEL (data->lsize), TRUE);
-#if !GTK_CHECK_VERSION(3,0,0)
-  gtk_misc_set_alignment (GTK_MISC (data->lsize), 0.0, 0.5);
-  gtk_table_attach (GTK_TABLE (t), data->lsize, 2, 3, 1, 2, GTK_FILL | GTK_EXPAND, 0, 0, 0);
-#else
   gtk_label_set_xalign (GTK_LABEL (data->lsize), 0.0);
   gtk_grid_attach (GTK_GRID (t), data->lsize, 2, 1, 1, 1);
   gtk_widget_set_hexpand (data->lsize, TRUE);
-#endif
 
   w = gtk_label_new (NULL);
   gtk_label_set_markup (GTK_LABEL (w), _("<b>Filename:</b>"));
-#if !GTK_CHECK_VERSION(3,0,0)
-  gtk_misc_set_alignment (GTK_MISC (w), 0.0, 0.5);
-  gtk_table_attach (GTK_TABLE (t), w, 1, 2, 2, 3, GTK_FILL, 0, 0, 0);
-#else
   gtk_label_set_xalign (GTK_LABEL (w), 0.0);
   gtk_grid_attach (GTK_GRID (t), w, 1, 2, 1, 1);
-#endif
+
   data->lfile = gtk_label_new (NULL);
   gtk_label_set_selectable (GTK_LABEL (data->lfile), TRUE);
-#if !GTK_CHECK_VERSION(3,0,0)
-  gtk_misc_set_alignment (GTK_MISC (data->lfile), 0.0, 0.5);
-  gtk_table_attach (GTK_TABLE (t), data->lfile, 2, 3, 2, 3, GTK_FILL | GTK_EXPAND, 0, 0, 0);
-#else
   gtk_label_set_xalign (GTK_LABEL (data->lfile), 0.0);
   gtk_grid_attach (GTK_GRID (t), data->lfile, 2, 2, 1, 1);
   gtk_widget_set_hexpand (data->lfile, TRUE);
-#endif
 
   /* create icon browser */
-#if !GTK_CHECK_VERSION(3,0,0)
-  p = gtk_hpaned_new ();
-#else
   p = gtk_paned_new (GTK_ORIENTATION_HORIZONTAL);
-#endif
   gtk_paned_set_position (GTK_PANED (p), 150);
   gtk_box_pack_start (GTK_BOX (box), p, TRUE, TRUE, 2);
 
@@ -357,9 +304,6 @@ main (gint argc, gchar * argv[])
 
   data->icon_list = gtk_tree_view_new ();
   gtk_tree_view_set_headers_visible (GTK_TREE_VIEW (data->icon_list), TRUE);
-#if !GTK_CHECK_VERSION(3,0,0)
-  gtk_tree_view_set_rules_hint (GTK_TREE_VIEW (data->icon_list), TRUE);
-#endif
   gtk_container_add (GTK_CONTAINER (w), data->icon_list);
 
   sel = gtk_tree_view_get_selection (GTK_TREE_VIEW (data->icon_list));

src/color.c
@@ -98,15 +98,9 @@ create_palette ()
 static void
 set_color (gchar *clr)
 {
-#if !GTK_CHECK_VERSION(3,4,0)
-      GdkColor c;
-      if (gdk_color_parse (clr, &c))
-        gtk_color_selection_set_current_color (GTK_COLOR_SELECTION (color), &c);
-#else
-      GdkRGBA c;
-      if (gdk_rgba_parse (&c, clr))
-        gtk_color_chooser_set_rgba (GTK_COLOR_CHOOSER (color), &c);
-#endif
+  GdkRGBA c;
+  if (gdk_rgba_parse (&c, clr))
+    gtk_color_chooser_set_rgba (GTK_COLOR_CHOOSER (color), &c);
 }
 
 static void
@@ -128,20 +122,11 @@ color_create_widget (GtkWidget * dlg)
 {
   GtkWidget *w;
 
-#if !GTK_CHECK_VERSION(3,4,0)
-  w = gtk_vbox_new (FALSE, 2);
-  color = gtk_color_selection_new ();
-#else
   w = gtk_box_new (GTK_ORIENTATION_VERTICAL, 2);
   color = gtk_color_chooser_widget_new ();
-#endif
-
   gtk_widget_set_name (color, "yad-color-widget");
-#if !GTK_CHECK_VERSION(3,4,0)
-  gtk_color_selection_set_has_palette (GTK_COLOR_SELECTION (color), options.color_data.gtk_palette);
-#else
   g_object_set (G_OBJECT (color), "show-editor", !options.color_data.gtk_palette, NULL);
-#endif
+
   if (options.color_data.init_color)
     set_color (options.color_data.init_color);
   gtk_box_pack_start (GTK_BOX (w), color, FALSE, FALSE, 2);
@@ -169,9 +154,7 @@ color_create_widget (GtkWidget * dlg)
           gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (sw), options.hscroll_policy, options.vscroll_policy);
           gtk_widget_set_size_request (sw, -1, 75);
           gtk_container_add (GTK_CONTAINER (exp), sw);
-#if GTK_CHECK_VERSION(3,0,0)
           gtk_widget_set_vexpand (sw, TRUE);
-#endif
           list = gtk_tree_view_new_with_model (model);
           gtk_widget_set_name (list, "yad-color-palette");
           g_object_unref (model);
@@ -218,22 +201,11 @@ color_create_widget (GtkWidget * dlg)
 void
 color_print_result (void)
 {
-#if !GTK_CHECK_VERSION(3,4,0)
-  GdkColor c;
-  guint64 alpha;
-#else
   GdkRGBA c;
-#endif
   gchar *cs;
 
-#if !GTK_CHECK_VERSION(3,4,0)
-  gtk_color_selection_get_current_color (GTK_COLOR_SELECTION (color), &c);
-  alpha = gtk_color_selection_get_current_alpha (GTK_COLOR_SELECTION (color));
-  cs = get_color (&c, alpha);
-#else
   gtk_color_chooser_get_rgba (GTK_COLOR_CHOOSER (color), &c);
   cs = get_color (&c);
-#endif
 
   if (cs)
     g_printf ("%s\n", cs);

src/entry.c
@@ -32,11 +32,7 @@ entry_activate_cb (GtkEntry * entry, gpointer data)
 static gboolean
 combo_activate_cb (GtkWidget * w, GdkEventKey * ev, gpointer data)
 {
-#if GTK_CHECK_VERSION(2,24,0)
   if (ev->keyval == GDK_KEY_Return || ev->keyval == GDK_KEY_KP_Enter)
-#else
-  if (ev->keyval == GDK_Return || ev->keyval == GDK_KP_Enter)
-#endif
     {
       if (options.plug == -1)
         yad_exit (options.data.def_resp);
@@ -115,11 +111,7 @@ entry_create_widget (GtkWidget * dlg)
 {
   GtkWidget *c, *l = NULL, *w = NULL;
 
-#if !GTK_CHECK_VERSION(3,0,0)
-  w = gtk_hbox_new (FALSE, 5);
-#else
   w = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 5);
-#endif
 
   if (options.entry_data.entry_label)
     {
@@ -198,11 +190,7 @@ entry_create_widget (GtkWidget * dlg)
 
       if (options.common_data.editable || settings.combo_always_editable)
         {
-#if GTK_CHECK_VERSION(2,24,0)
           c = gtk_combo_box_text_new_with_entry ();
-#else
-          c = gtk_combo_box_entry_new_text ();
-#endif
           gtk_widget_set_name (c, "yad-entry-edit-combo");
           entry = gtk_bin_get_child (GTK_BIN (c));
           if (options.entry_data.licon)
@@ -222,11 +210,7 @@ entry_create_widget (GtkWidget * dlg)
         }
       else
         {
-#if GTK_CHECK_VERSION(2,24,0)
           c = entry = gtk_combo_box_text_new ();
-#else
-          c = entry = gtk_combo_box_new_text ();
-#endif
           gtk_widget_set_name (c, "yad-entry-combo");
           is_combo = TRUE;
         }
@@ -238,22 +222,12 @@ entry_create_widget (GtkWidget * dlg)
           if (options.entry_data.entry_text &&
               g_ascii_strcasecmp (options.extra_data[i], options.entry_data.entry_text) == 0)
             active = i;
-#if GTK_CHECK_VERSION(2,24,0)
           gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (c), options.extra_data[i]);
-#else
-          gtk_combo_box_append_text (GTK_COMBO_BOX (c), options.extra_data[i]);
-#endif
           i++;
         }
 
       if (options.entry_data.entry_text && active == -1)
-        {
-#if GTK_CHECK_VERSION(2,24,0)
-          gtk_combo_box_text_prepend_text (GTK_COMBO_BOX_TEXT (c), options.entry_data.entry_text);
-#else
-          gtk_combo_box_prepend_text (GTK_COMBO_BOX (c), options.entry_data.entry_text);
-#endif
-        }
+        gtk_combo_box_text_prepend_text (GTK_COMBO_BOX_TEXT (c), options.entry_data.entry_text);
 
       /* set first iter active */
       if (!options.common_data.editable)
@@ -337,13 +311,7 @@ entry_print_result (void)
       if (options.common_data.num_output)
         g_print ("%d\n", gtk_combo_box_get_active (GTK_COMBO_BOX (entry)) + 1);
       else
-        {
-#if GTK_CHECK_VERSION(2,24,0)
-          g_print ("%s\n", gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT (entry)));
-#else
-          g_print ("%s\n", gtk_combo_box_get_active_text (GTK_COMBO_BOX (entry)));
-#endif
-        }
+        g_print ("%s\n", gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT (entry)));
     }
   else
     g_print ("%s\n", gtk_entry_get_text (GTK_ENTRY (entry)));

src/font.c
@@ -28,11 +28,7 @@ static GtkWidget *font;
 static void
 realize_cb (GtkWidget * w, gpointer d)
 {
-#if GTK_CHECK_VERSION(3,2,0)
   gtk_font_chooser_set_font (GTK_FONT_CHOOSER (w), options.common_data.font);
-#else
-  gtk_font_selection_set_font_name (GTK_FONT_SELECTION (w), options.common_data.font);
-#endif
 }
 
 GtkWidget *
@@ -40,19 +36,11 @@ font_create_widget (GtkWidget * dlg)
 {
   GtkWidget *w;
 
-#if GTK_CHECK_VERSION(3,2,0)
   w = font = gtk_font_chooser_widget_new ();
-#else
-  w = font = gtk_font_selection_new ();
-#endif
   gtk_widget_set_name (w, "yad-font-widget");
 
   if (options.font_data.preview)
-#if GTK_CHECK_VERSION(3,2,0)
     gtk_font_chooser_set_preview_text (GTK_FONT_CHOOSER (w), options.font_data.preview);
-#else
-    gtk_font_selection_set_preview_text (GTK_FONT_SELECTION (w), options.font_data.preview);
-#endif
 
   /* font must be set after widget inserted in toplevel */
   if (options.common_data.font)
@@ -70,15 +58,9 @@ font_print_result (void)
       PangoFontFamily *family;
       gint size;
 
-#if GTK_CHECK_VERSION(3,2,0)
       face = gtk_font_chooser_get_font_face (GTK_FONT_CHOOSER (font));
       family = gtk_font_chooser_get_font_family (GTK_FONT_CHOOSER (font));
       size = gtk_font_chooser_get_font_size (GTK_FONT_CHOOSER (font));
-#else
-      face = gtk_font_selection_get_face (GTK_FONT_SELECTION (font));
-      family = gtk_font_selection_get_family (GTK_FONT_SELECTION (font));
-      size = gtk_font_selection_get_size (GTK_FONT_SELECTION (font));
-#endif
 
       if (options.common_data.quoted_output)
         {
@@ -99,11 +81,7 @@ font_print_result (void)
     }
   else
     {
-#if GTK_CHECK_VERSION(3,2,0)
       gchar *fn = gtk_font_chooser_get_font (GTK_FONT_CHOOSER (font));
-#else
-      gchar *fn = gtk_font_selection_get_font_name (GTK_FONT_SELECTION (font));
-#endif
 
       if (options.common_data.quoted_output)
         {

commit 1fb1b17e20adaa99fe3b7b4ac98aa70fd3227a36

fix blinking notification icon regression GTK2
Originated in 26c27b46779d886c3de6f309777f539bbe383e5e.

GTK3 has removed support for blinking notification icon.

data/yad.1
@@ -1061,7 +1061,7 @@ Set notification icon to ICONNAME.
 Set notification tooltip.
 .TP
 .B visible:[true|false|blink]
-Set notification icon to visible, invisible or blinking states.
+Set notification icon to visible, invisible or (GTK+-2 only) blinking state.
 .TP
 .B action:COMMAND
 Specify the command running when click on the icon.

src/notification.c
@@ -327,10 +327,28 @@ handle_stdin (GIOChannel * channel, GIOCondition condition, gpointer data)
           else if (!g_ascii_strcasecmp (command, "visible"))
             {
               G_GNUC_BEGIN_IGNORE_DEPRECATIONS;
-              if (!g_ascii_strcasecmp (value, "false"))
-                gtk_status_icon_set_visible (status_icon, FALSE);
+#if !GTK_CHECK_VERSION(3,0,0)
+              if (!g_ascii_strcasecmp (value, "blink"))
+                {
+                  gboolean state = gtk_status_icon_get_blinking (status_icon);
+                  gtk_status_icon_set_blinking (status_icon, !state);
+                }
               else
-                gtk_status_icon_set_visible (status_icon, TRUE);
+#endif
+              if (!g_ascii_strcasecmp (value, "false"))
+                {
+                  gtk_status_icon_set_visible (status_icon, FALSE);
+#if !GTK_CHECK_VERSION(3,0,0)
+                  gtk_status_icon_set_blinking (status_icon, FALSE);
+#endif
+                }
+              else
+                {
+                  gtk_status_icon_set_visible (status_icon, TRUE);
+#if !GTK_CHECK_VERSION(3,0,0)
+                  gtk_status_icon_set_blinking (status_icon, FALSE);
+#endif
+                }
               G_GNUC_END_IGNORE_DEPRECATIONS;
             }
           else if (!g_ascii_strcasecmp (command, "action"))