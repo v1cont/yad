b504029a72cee21436704187e42fb95df16ae8ab
Dave Shoreman <dshoreman@users.noreply.github.com>
2026-02-25 12:10:23 +0000

    Actionless indicator should quit by default
    Restores middle-click-quit by default (unless `--no-middle` is set) and
    removes the automatic Quit menu item to restore original --notification
    behaviour and avoid existing menus getting duplicated Quit items.

--- src/appindicator.c
+++ src/appindicator.c
@@ -177,7 +177,7 @@ build_indicator_menu (void)
   indicator_menu = gtk_menu_new ();
 
   /* Register secondary (middle-click) action */
-  if (action && g_ascii_strcasecmp (action, "quit") != 0 && g_ascii_strcasecmp (action, "menu") != 0)
+  if (action && g_ascii_strcasecmp (action, "menu") != 0)
     {
       item = gtk_menu_item_new ();
       g_signal_connect (item, "activate", G_CALLBACK (activate_cb), NULL);
@@ -222,14 +222,6 @@ build_indicator_menu (void)
 
       gtk_menu_shell_append (GTK_MENU_SHELL (indicator_menu), item);
     }
-
-  /* Add quit item */
-  item = gtk_separator_menu_item_new ();
-  gtk_menu_shell_append (GTK_MENU_SHELL (indicator_menu), item);
-
-  item = gtk_menu_item_new_with_label (_("Quit"));
-  g_signal_connect (item, "activate", G_CALLBACK (popup_menu_item_activate_cb), "quit");
-  gtk_menu_shell_append (GTK_MENU_SHELL (indicator_menu), item);
 
   gtk_widget_show_all (indicator_menu);
   app_indicator_set_menu (app_indicator, GTK_MENU (indicator_menu));
@@ -359,6 +351,8 @@ yad_appindicator_run (void)
     icon = g_strdup (options.data.dialog_image);
   if (options.common_data.command)
     action = g_strdup (options.common_data.command);
+  else if (options.notification_data.middle)
+    action = "quit";
 
   if (options.notification_data.menu)
     parse_menu_str (options.notification_data.menu);
